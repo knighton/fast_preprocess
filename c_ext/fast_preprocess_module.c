05ccea0c6182fc5762b73d088275d878f9d700e5
James Knighton <jknighton@livefyre.com>
2015-04-10 12:46:43 -0700

    destutter_unicode()

--- c_ext/fast_preprocess_module.c
+++ c_ext/fast_preprocess_module.c
@@ -3,7 +3,14 @@
 #include "unicode.h"
 
 static char FAST_PREPROCESS_DOC[] =
-    "C++ extension that does preprocessing.";
+    "C++ extension that does preprocessing.\n"
+    "\n"
+    "Methods that operate on Unicode objects:\n"
+    "- destutter_unicode\n"
+    "\n"
+    "Methods that operate on raw bytes:\n"
+    "- destutter_ascii_bytes\n"
+    "- destutter_utf8_encoded_bytes\n";
 
 bool increment(PyObject* key2count, PyObject* key) {
     PyObject* value = PyDict_GetItem(key2count, key);
@@ -16,13 +23,67 @@ bool increment(PyObject* key2count, PyObject* key) {
     return !PyDict_SetItem(key2count, key, new_value);
 }
 
-static char DESTUTTER_ASCII_STR_DOC[] =
+static char DESTUTTER_UNICODE_DOC[] =
+    "(unicode, max repeat, dict) -> unicode without excessive repeats.\n"
+    "\n"
+    "Saves code point -> drop count to the dict.";
+
+static PyObject* destutter_unicode(PyObject* self, PyObject* args) {
+    Py_UNICODE* input_str;
+    int input_str_len;
+    int max_repeat_count;
+    PyObject* chr2dropcount;
+    if (!PyArg_ParseTuple(
+            args, "u#IO!", &input_str, &input_str_len, &max_repeat_count,
+            &PyDict_Type, &chr2dropcount)) {
+        return NULL;
+    }
+
+    if (!input_str_len) {
+        return PyUnicode_FromStringAndSize("", 0);
+    }
+
+    Py_UNICODE prev_c = input_str[0];
+    int run_length = 1;
+    Py_UNICODE* r = (Py_UNICODE*)malloc(input_str_len * sizeof(Py_UNICODE));
+    int r_used = 0;
+    r[r_used++] = input_str[0];
+    int i;
+    for (i = 1; i < input_str_len; ++i) {
+        Py_UNICODE c = input_str[i];
+
+        if (c != prev_c) {
+            prev_c = c;
+            run_length = 1;
+            r[r_used++] = c;
+            continue;
+        }
+
+        ++run_length;
+        if (run_length <= max_repeat_count) {
+            r[r_used++] = c;
+            continue;
+        }
+
+        PyObject* key = PyUnicode_FromUnicode(&c, 1);
+        if (!increment(chr2dropcount, key)) {
+            free(r);
+            return NULL;
+        }
+    }
+
+    PyObject* ret = PyUnicode_FromUnicode(r, r_used);
+    free(r);
+    return ret;
+}
+
+static char DESTUTTER_ASCII_BYTES_DOC[] =
     "(ASCII string, max char repeat count, dict) -> string with excessive "
     "repeats removed.\n"
     "\n"
     "Saves code point -> drop count to the dict.";
 
-static PyObject* destutter_ascii_str(PyObject* self, PyObject* args) {
+static PyObject* destutter_ascii_bytes(PyObject* self, PyObject* args) {
     char* input_str;
     int input_str_len;
     int max_repeat_count;
@@ -71,13 +132,13 @@ static PyObject* destutter_ascii_str(PyObject* self, PyObject* args) {
     return ret;
 }
 
-static char DESTUTTER_UTF8_ENCODED_STR_DOC[] =
+static char DESTUTTER_UTF8_ENCODED_BYTES_DOC[] =
     "(raw bytes of a UTF-8 string, max chr repeat count, dict) -> string with "
     "excessive repeats removed.\n"
     "\n"
     "Saves code point -> drop count to the dict.";
 
-static PyObject* destutter_utf8_encoded_str(PyObject* self, PyObject* args) {
+static PyObject* destutter_utf8_encoded_bytes(PyObject* self, PyObject* args) {
     char* input_str;
     int input_str_len;
     int max_repeat_count;
@@ -139,10 +200,12 @@ static PyObject* destutter_utf8_encoded_str(PyObject* self, PyObject* args) {
 }
 
 static PyMethodDef FAST_PREPROCESS_METHODS[] = {
-    {"destutter_ascii_str", destutter_ascii_str, METH_VARARGS,
-     DESTUTTER_ASCII_STR_DOC},
-    {"destutter_utf8_encoded_str", destutter_utf8_encoded_str, METH_VARARGS,
-     DESTUTTER_UTF8_ENCODED_STR_DOC},
+    {"destutter_unicode", destutter_unicode, METH_VARARGS,
+     DESTUTTER_UNICODE_DOC},
+    {"destutter_ascii_bytes", destutter_ascii_bytes, METH_VARARGS,
+     DESTUTTER_ASCII_BYTES_DOC},
+    {"destutter_utf8_encoded_bytes", destutter_utf8_encoded_bytes, METH_VARARGS,
+     DESTUTTER_UTF8_ENCODED_BYTES_DOC},
     {NULL, NULL, 0, NULL},
 };
 
--- go.py
+++ go.py
@@ -1,15 +1,34 @@
 # -*- encoding: utf-8 -*-
 
-from fast_preprocess import destutter_utf8_encoded_str
+from fast_preprocess import (destutter_unicode, destutter_utf8_encoded_bytes)
 
 
-asc = 'abcccdddccce'
-uni = '☃☃☃☃☃☃ fuuuuu'
-uni2 = u'\U0001f4f2\U0001f4f2\U0001f4f2\U0001f4f2\U0001f4f2\U0001f4f2\U0001f4f2\U0001f4f2'.encode('utf-8')
-max_repeat = 2
+def show_bytes(s, max_repeat, new_s, drop_counts):
+    print '%s, max_repeat=%d -> %s, %s' % (s, max_repeat, new_s, drop_counts)
 
-for f in [destutter_utf8_encoded_str]:
-    for s in [asc, uni, uni2]:
+
+def show_unicode(u, max_repeat, new_u, drop_counts):
+    print '%s, max_repeat=%d -> %s, %s' % (u, max_repeat, new_u, drop_counts)
+
+
+def main():
+    max_repeat = 2
+
+    asc = 'abcccdddccce'
+    uni = '☃☃☃☃☃☃ fuuuuu'
+    uni2 = u'\U0001f4f2\U0001f4f2\U0001f4f2\U0001f4f2\U0001f4f2\U0001f4f2\U0001f4f2\U0001f4f2'.encode('utf-8')
+
+    for f in [destutter_utf8_encoded_bytes]:
+        for s in [asc, uni, uni2]:
+            drop_counts = {}
+            new_s = f(s, max_repeat, drop_counts)
+            show_bytes(s, max_repeat, new_s, drop_counts)
+
+    for u in map(lambda s: s.decode('utf-8'), [asc, uni, uni2]):
         drop_counts = {}
-        new_s = f(s, max_repeat, drop_counts)
-        print s, max_repeat, '->', new_s, drop_counts
+        new_u = destutter_unicode(u, max_repeat, drop_counts)
+        show_unicode(u, max_repeat, new_u, drop_counts)
+
+
+if __name__ == '__main__':
+    main()
