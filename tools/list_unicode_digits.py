be66dee3a26f692685bc080b5e51977b5b96f385
James Knighton <jknighton@livefyre.com>
2015-04-10 14:04:00 -0700

    tools/list_unicode_digits().

---
+++ tools/list_unicode_digits.py
@@ -0,0 +1,19 @@
+"""
+Dump every digit-like unicode character.
+"""
+
+import regex
+import sys
+
+
+def main():
+    re = regex.compile(u'\\d', regex.UNICODE)
+
+    for i in xrange(sys.maxunicode):
+        c = unichr(i)
+        if re.search(c):
+            print i
+
+
+if __name__ == '__main__':
+    main()
