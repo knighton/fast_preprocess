97b90e53a6a4c01e924a544c7b1161a3eba789dd
James Knighton <jknighton@livefyre.com>
2015-04-09 23:46:09 -0700

    Implement strip_excessive_repeats_utf().

--- cc_ext/fast_preprocess_module.cpp
+++ cc_ext/fast_preprocess_module.cpp
@@ -2,20 +2,31 @@
 #include <Python.h>
 #include <string>
 
-#include "utf8.h"
+#include "unicode.h"
 
 using std::string;
 
 static char FAST_PREPROCESS_DOC[] =
     "C++ extension that does preprocessing.";
 
-static char STRIP_EXCESSIVE_REPEATS_DOC[] =
-    "(string, max repeat count, dict) -> string with excessive repeats dropped.\n"
+bool increment(PyObject* key2count, PyObject* key) {
+    PyObject* value = PyDict_GetItem(key2count, key);
+    PyObject* new_value;
+    if (!value) {
+        new_value = PyInt_FromLong(1);
+    } else {
+        new_value = PyInt_FromLong(PyInt_AS_LONG(value) + 1);
+    }
+    return !PyDict_SetItem(key2count, key, new_value);
+}
+
+static char STRIP_EXCESSIVE_REPEATS_ASCII_DOC[] =
+    "(ASCII string, max char repeat count, dict) -> string with excessive "
+    "repeats removed.\n"
     "\n"
     "Saves code point -> drop count to the dict.";
 
-static PyObject* strip_excessive_repeats(PyObject* self, PyObject* args) {
-    // Extract args (input string, max repeat count, chr -> drop count dict).
+static PyObject* strip_excessive_repeats_ascii(PyObject* self, PyObject* args) {
     char* input_str;
     int input_str_len;
     int max_repeat_count;
@@ -26,43 +37,31 @@ static PyObject* strip_excessive_repeats(PyObject* self, PyObject* args) {
         return NULL;
     }
 
-    // If input string is empty, we're done.
     if (!input_str_len) {
         return PyString_FromString("");
     }
 
-    // For each character,
-    char prev_chr = input_str[0];
+    char prev_c = input_str[0];
     int run_length = 1;
     string r;
     for (int i = 1; i < input_str_len; ++i) {
         char c = input_str[i];
 
-        // If this character is different, reset state.
-        if (c != prev_chr) {
-            prev_chr = c;
+        if (c != prev_c) {
+            prev_c = c;
             run_length = 1;
             r += c;
             continue;
         }
 
-        // If it's the same, we're done if the run isn't too long.
         ++run_length;
         if (run_length <= max_repeat_count) {
             r += c;
             continue;
         }
 
-        // Note excessive repeats.
-        PyObject* key = PyInt_FromLong(c);
-        PyObject* value = PyDict_GetItem(chr2dropcount, key);
-        PyObject* new_value;
-        if (!value) {
-            new_value = PyInt_FromLong(1);
-        } else {
-            new_value = PyInt_FromLong(PyInt_AS_LONG(value) + 1);
-        }
-        if (PyDict_SetItem(chr2dropcount, key, new_value)) {
+        PyObject* key = PyString_FromFormat("%c", c);
+        if (!increment(chr2dropcount, key)) {
             return NULL;
         }
     }
@@ -70,9 +69,68 @@ static PyObject* strip_excessive_repeats(PyObject* self, PyObject* args) {
     return PyString_FromStringAndSize(r.data(), r.size());
 }
 
+static char STRIP_EXCESSIVE_REPEATS_UTF8_DOC[] =
+    "(raw bytes of a UTF-8 string, max chr repeat count, dict) -> string with "
+    "excessive repeats removed.\n"
+    "\n"
+    "Saves code point -> drop count to the dict.";
+
+static PyObject* strip_excessive_repeats_utf8(PyObject* self, PyObject* args) {
+    char* input_str;
+    int input_str_len;
+    int max_repeat_count;
+    PyObject* chr2dropcount;
+    if (!PyArg_ParseTuple(
+            args, "s#IO!", &input_str, &input_str_len, &max_repeat_count,
+            &PyDict_Type, &chr2dropcount)) {
+        return NULL;
+    }
+
+    if (!input_str_len) {
+        return PyString_FromString("");
+    }
+
+    string input_s = string(input_str, input_str_len);
+    size_t byte_index = 0;
+    unicode::CodePoint c;
+    if (!unicode::ReadNextUTF8(input_s, &byte_index, &c)) {
+        return PyString_FromString("");
+    }
+
+    unicode::CodePoint prev_c = c;
+    int run_length = 1;
+    string r;
+    bool loop;
+    do {
+        loop = unicode::ReadNextUTF8(input_s, &byte_index, &c);
+
+        if (c != prev_c) {
+            prev_c = c;
+            run_length = 1;
+            r += c;
+            continue;
+        }
+
+        ++run_length;
+        if (run_length <= max_repeat_count) {
+            r += c;
+            continue;
+        }
+
+        PyObject* key = PyInt_FromLong(c);
+        if (!increment(chr2dropcount, key)) {
+            return NULL;
+        }
+    } while (loop);
+
+    return PyString_FromStringAndSize(r.data(), r.size());
+}
+
 static PyMethodDef FAST_PREPROCESS_METHODS[] = {
-    {"strip_excessive_repeats", strip_excessive_repeats, METH_VARARGS,
-     STRIP_EXCESSIVE_REPEATS_DOC},
+    {"strip_excessive_repeats_ascii", strip_excessive_repeats_ascii,
+     METH_VARARGS, STRIP_EXCESSIVE_REPEATS_ASCII_DOC},
+    {"strip_excessive_repeats_utf8", strip_excessive_repeats_utf8,
+     METH_VARARGS, STRIP_EXCESSIVE_REPEATS_UTF8_DOC},
     {NULL, NULL, 0, NULL},
 };
 
--- go.py
+++ go.py
@@ -1,12 +1,13 @@
 import json
 
-from fast_preprocess import strip_excessive_repeats
+from fast_preprocess import (strip_excessive_repeats_ascii,
+                             strip_excessive_repeats_utf8)
 
 
 s = 'abcccdddccce'
 max_repeat = 2
-drop_counts = {}
-new_s = strip_excessive_repeats(s, max_repeat, drop_counts)
-print s, max_repeat, drop_counts
-print new_s
-print json.dumps(drop_counts, indent=4)
+
+for f in [strip_excessive_repeats_ascii, strip_excessive_repeats_utf8]:
+    drop_counts = {}
+    new_s = f(s, max_repeat, drop_counts)
+    print s, max_repeat, '->', new_s, drop_counts
