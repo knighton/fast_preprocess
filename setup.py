4a4652686429bb4287bd56eb022b13a195744d58
James Knighton <jknighton@livefyre.com>
2015-04-10 11:48:28 -0700

    Kill the C++ extension.

--- setup.py
+++ setup.py
@@ -7,15 +7,7 @@ c_ext = Extension(
         'c_ext/unicode.c',
     ]
 )
-
-cpp_ext = Extension(
-    'fast_preprocess_cpp',
-    [
-        'cpp_ext/fast_preprocess_module.cpp',
-        'cpp_ext/unicode.cpp',
-    ]
-)
  
 setup(
-    ext_modules=[c_ext, cpp_ext],
+    ext_modules=[c_ext],
 )
